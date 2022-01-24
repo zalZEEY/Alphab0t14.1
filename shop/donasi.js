3b15325da2a3f7ec3fadca8b32d673f1b1943488
Z E E Y <88220322+zalZEEY@users.noreply.github.com>
2022-01-24 21:23:02 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -4,24 +4,26 @@ return `DONASI BISA VIA BANK / E-MONEY
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ - 
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ - 
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ - 
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ - 
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ - 
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ - 
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ - 
+4. SAWERIA
+	ㅁ https://saweria.co/SADZeey
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama Z E E Y
 `
 	}
 
